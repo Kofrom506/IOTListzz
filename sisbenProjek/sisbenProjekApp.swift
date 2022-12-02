db9037cfbe701c59117b8424e0bfdcf90daa487f
Kofrom506 <71697178+Kofrom506@users.noreply.github.com>
2022-12-02 13:43:19 +0700

    Initial Commit

---
+++ sisbenProjek/sisbenProjekApp.swift
@@ -0,0 +1,17 @@
+//
+//  sisbenProjekApp.swift
+//  sisbenProjek
+//
+//  Created by Evan Susanto on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sisbenProjekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
