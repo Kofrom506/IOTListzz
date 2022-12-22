57125bb81693b1c3ae33ac5acd3e367d1de4735c
Kofrom506 <71697178+Kofrom506@users.noreply.github.com>
2022-12-22 21:47:19 +0700

    first commit

---
+++ Pods/Target Support Files/Pods-sisbenProjek/Pods-sisbenProjek-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sisbenProjek : NSObject
+@end
+@implementation PodsDummy_Pods_sisbenProjek
+@end
--- sisbenProjek/sisbenProjekApp.swift
+++ sisbenProjek/sisbenProjekApp.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
+import Firebase
+
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct sisbenProjekApp: App {
+//    let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var selectedTab: Tab = .homeViewPage
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginPageView()
         }
     }
 }
+struct mainView: View{
+    @State private var selectedTab: Tab = .homeViewPage
+    var body: some View{
+        ZStack{
+            switch(self.selectedTab.page){
+            case 1:
+                HomePageView()
+            case 2:
+                ScanPageView()
+            case 3:
+                StudentListView()
+            case 4:
+                ProfileView()
+            default:
+                StudentListView()
+            }
+
+            VStack{
+                Spacer()
+                TabViewCustom(selectedTab: $selectedTab)
+            }
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+//struct sisbenProjekApp_Previews: PreviewProvider {secure
+//    static var previews: some View {
+//        sisbenProjekApp()
+//            .environmentObject(HomePageView())
+//    }
+//}
